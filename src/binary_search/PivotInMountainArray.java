b1382e8f553fd2f397bd9f7b9c840d389befdace
Pranjal <p.arudiya9@gmail.com>
2024-10-27 19:20:52 +0530

    BS - Pivot in mountain array

---
+++ src/binary_search/PivotInMountainArray.java
@@ -0,0 +1,23 @@
+package src.binary_search;
+
+public class PivotInMountainArray {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1};
+        int pivot = getPivot(arr);
+        System.out.println("Pivot index is: " + pivot);
+    }
+
+    private static int getPivot(int[] arr) {
+        int s = 0, e = arr.length - 1;
+        while (s < e) {
+            int mid = s + (e - s)/2;
+            if(arr[mid] < arr[mid + 1]) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+        return e;
+    }
+}
